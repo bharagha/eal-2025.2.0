4729da7771d578cebd0076320615fe781f316b51
Oskar Fiedot <oskar.fiedot@intel.com>
2025-09-18 08:24:11 +0000

    Add support for gst analytics metadata in gvapython

--- libraries/dl-streamer/python/gstgva/region_of_interest.py
+++ libraries/dl-streamer/python/gstgva/region_of_interest.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Copyright (C) 2018-2021 Intel Corporation
+# Copyright (C) 2018-2025 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 # ==============================================================================
@@ -9,7 +9,7 @@
 
 import ctypes
 import numpy
-from typing import List
+from typing import List, Optional
 from collections import namedtuple
 
 from .tensor import Tensor
@@ -17,12 +17,15 @@ from .util import VideoRegionOfInterestMeta
 from .util import libgst, libgobject, libgstvideo, GLIST_POINTER
 
 import gi
-gi.require_version('GstVideo', '1.0')
-gi.require_version('GLib', '2.0')
-gi.require_version('Gst', '1.0')
-from gi.repository import GstVideo, GLib, GObject, Gst
+
+gi.require_version("GstVideo", "1.0")
+gi.require_version("GLib", "2.0")
+gi.require_version("Gst", "1.0")
+gi.require_version("GstAnalytics", "1.0")
+from gi.repository import GstVideo, GLib, GObject, Gst, GstAnalytics
 
 Rect = namedtuple("Rect", "x y w h")
+
 
 ## @brief This class represents region of interest - object describing detection result (bounding box) and containing
 # multiple Tensor objects (inference results) attached by multiple models. For example, it can be region of interest with detected face and recognized age
@@ -30,78 +33,167 @@ Rect = namedtuple("Rect", "x y w h")
 # elements with two classification models. Such RegionOfInterest will have bounding box coordinates filled and will have 3 Tensor objects
 # attached - 1 Tensor object with detection result and 2 Tensor objects with classification results coming from 2 classifications
 class RegionOfInterest(object):
+
+    def __init__(
+        self, od_meta: GstAnalytics.ODMtd, roi_meta: VideoRegionOfInterestMeta
+    ):
+        self.__roi_meta = roi_meta
+        self.__od_meta = od_meta
+        self._detection = None
+        self._tensors = []
+
+        # Load existing tensors from ROI meta
+        if roi_meta and roi_meta._params:
+            param = roi_meta._params
+            while param:
+                tensor_structure = param.contents.data
+                tensor = Tensor(tensor_structure)
+                if (
+                    tensor.name() != "object_id"
+                    and tensor.type() != "classification_result"
+                ):
+                    self._tensors.append(tensor)
+                    if tensor.is_detection():
+                        self._detection = tensor
+                param = param.contents.next
+
+        for rlt_mtd in od_meta.iter_direct_related(GstAnalytics.RelTypes.CONTAIN):
+            tensor_structure = Tensor.convert_to_tensor(rlt_mtd)
+            if tensor_structure is not None:
+                tensor = Tensor(tensor_structure)
+                self._tensors.append(tensor)
+
     ## @brief Get bounding box of the RegionOfInterest as pixel coordinates in original image
     #  @return Bounding box coordinates of the RegionOfInterest
     def rect(self):
-        return Rect(x = self.__roi_meta.x,
-                    y = self.__roi_meta.y,
-                    w = self.__roi_meta.w,
-                    h = self.__roi_meta.h)
+        success, x, y, w, h, _, _ = self.__od_meta.get_oriented_location()
+
+        if not success:
+            raise RuntimeError(
+                "RegionOfInterest:rect: Failed to get oriented location from analytics metadata"
+            )
+
+        return Rect(x=x, y=y, w=w, h=h)
 
     ## @brief Get bounding box of the RegionOfInterest as normalized coordinates in the range [0, 1]
     #  @return Bounding box coordinates of the RegionOfInterest
     def normalized_rect(self):
         detection = self.detection()
-        return Rect(x = detection['x_min'],
-                    y = detection['y_min'],
-                    w = detection['x_max'] - detection['x_min'],
-                    h = detection['y_max'] - detection['y_min'])
+        return Rect(
+            x=detection["x_min"],
+            y=detection["y_min"],
+            w=detection["x_max"] - detection["x_min"],
+            h=detection["y_max"] - detection["y_min"],
+        )
+
+    ## @brief Get bounding box rotation
+    #  @return Bounding box rotation of the RegionOfInterest
+    def rotation(self) -> float:
+        """Get RegionOfInterest bounding box rotation using analytics metadata."""
+        success, _, _, _, _, r, _ = self.__od_meta.get_oriented_location()
+
+        if not success:
+            raise RuntimeError(
+                "RegionOfInterest:rotation: Failed to get oriented location from analytics metadata"
+            )
+
+        if r >= 0.0:
+            return r
+
+        return 0.0
 
     ## @brief Get class label of this RegionOfInterest
     #  @return Class label of this RegionOfInterest
     def label(self) -> str:
-        return GLib.quark_to_string(self.__roi_meta.roi_type)
+        label_quark = self.__od_meta.get_obj_type()
 
-    ## @brief Get confidence from detection Tensor, last added to this RegionOfInterest
-    # @return last added detection Tensor confidence if exists, otherwise None
+        if label_quark:
+            return GLib.quark_to_string(label_quark)
+
+        return ""
+
+    ## @brief Get confidence from detection using analytics metadata
+    # @return detection confidence if exists, otherwise None
     def confidence(self) -> float:
-        detection = self.detection()
-        return detection.confidence() if detection else None
+        success, confidence = self.__od_meta.get_confidence_lvl()
 
-    ## @brief Get object id
+        if not success:
+            raise RuntimeError(
+                "RegionOfInterest:confidence: Failed to get confidence level from analytics metadata"
+            )
+
+        if confidence >= 0.0:
+            return confidence
+
+        return 0.0
+
+    ## @brief Get object id using analytics tracking metadata
     # @return object id as an int, None if failed to get
     def object_id(self) -> int:
-        param = self.meta()._params
-        object_id_tensor = None
-        while param:
-            tensor_structure = param.contents.data
-            if libgst.gst_structure_has_name(tensor_structure, "object_id".encode('utf-8')):
-                object_id_tensor = Tensor(tensor_structure)
-                break
-            param = param.contents.next
-        if object_id_tensor == None:
-            return None
-        return object_id_tensor['id']
+        for trk_mtd in self.__od_meta.iter_direct_related(
+            GstAnalytics.RelTypes.ANY, GstAnalytics.TrackingMtd
+        ):
+            success, tracking_id, _, _, _ = trk_mtd.get_info()
 
-    ## @brief Set object id
+            if not success:
+                raise RuntimeError(
+                    "RegionOfInterest:object_id: Failed to get tracking info from analytics metadata"
+                )
+
+            return tracking_id
+
+        return 0
+
+    ## @brief Set object id using analytics tracking metadata
     # @return None
     def set_object_id(self, object_id: int):
-        object_id_tensor_name = "object_id"
-        object_id_tensor = None
-        param = self.meta()._params
-        while param:
-            tensor_structure = param.contents.data
-            if not libgst.gst_structure_has_name(tensor_structure, object_id_tensor_name.encode('utf-8')):
-                param = param.contents.next
-                continue
-            object_id_tensor = Tensor(tensor_structure)
-            break
+        # Set in ROI meta for backward compatibility
+        if self.meta():
+            s_object_id = libgstvideo.gst_video_region_of_interest_meta_get_param(
+                self.meta(), "object_id".encode("utf-8")
+            )
 
-        if object_id_tensor is None:
-            object_id_tensor = self.add_tensor(object_id_tensor_name)
+            if s_object_id:
+                tensor_object_id = Tensor(s_object_id)
+                tensor_object_id["id"] = object_id
+            else:
+                tensor_structure = libgst.gst_structure_new_empty(
+                    "object_id".encode("utf-8")
+                )
+                tensor_object_id = Tensor(tensor_structure)
+                tensor_object_id["id"] = object_id
+                libgstvideo.gst_video_region_of_interest_meta_add_param(
+                    self.meta(), tensor_structure
+                )
 
-        object_id_tensor["id"] = object_id
+        for trk_mtd in self.__od_meta.iter_direct_related(
+            GstAnalytics.RelTypes.ANY, GstAnalytics.TrackingMtd
+        ):
+            if not self.__od_meta.meta.set_relation(
+                GstAnalytics.RelTypes.NONE, self.__od_meta.id, trk_mtd.id
+            ):
+                raise RuntimeError(
+                    "RegionOfInterest:set_object_id: Failed to remove existing relation to tracking metadata"
+                )
+
+        success, trk_mtd = self.__od_meta.meta.add_tracking_mtd(object_id, 0)
+
+        if not success:
+            raise RuntimeError(
+                "RegionOfInterest:set_object_id: Failed to add tracking metadata"
+            )
+
+        if not self.__od_meta.meta.set_relation(
+            GstAnalytics.RelTypes.RELATE_TO, self.__od_meta.id, trk_mtd.id
+        ):
+            raise RuntimeError(
+                "RegionOfInterest:set_object_id: Failed to set relation to tracking metadata"
+            )
 
     ## @brief Get all Tensor instances added to this RegionOfInterest
     # @return list of Tensor instances added to this RegionOfInterest
     def tensors(self):
-        param = self.meta()._params
-        while param:
-            tensor_structure = param.contents.data
-            # "object_id" is used to store ROI id for tracking
-            if not libgst.gst_structure_has_name(tensor_structure, "object_id".encode('utf-8')):
-                yield Tensor(tensor_structure)
-            param = param.contents.next
+        return self._tensors
 
     ## @brief Returns detection Tensor, last added to this RegionOfInterest. As any other Tensor, returned detection
     # Tensor can contain arbitrary information. If you use RegionOfInterest based on VideoRegionOfInterestMeta
@@ -111,32 +203,65 @@ class RegionOfInterest(object):
     # @return detection Tensor, empty if there were no detection Tensor objects added to this RegionOfInterest when
     # this method was called
     def detection(self) -> Tensor:
-        for tensor in self.tensors():
-            if tensor.is_detection():
-                return tensor
-        return self.add_tensor('detection')
+        if not self._detection:
+            gst_structure = libgst.gst_structure_new_empty("detection".encode("utf-8"))
+            detection_tensor = Tensor(gst_structure)
+            self.add_tensor(detection_tensor)
+        return self._detection
 
-    ## @brief Get label_id from detection Tensor, last added to this RegionOfInterest
-    # @return last added detection Tensor label_id if exists, otherwise None
+    ## @brief Get label_id from analytics metadata or detection Tensor
+    # @return label_id if exists, otherwise 0
     def label_id(self) -> int:
-        detection = self.detection()
-        return detection.label_id() if detection else None
+        label_quark = self.__od_meta.get_obj_type()
+
+        cls_descriptor_mtd = next(
+            self.__od_meta.iter_direct_related(
+                GstAnalytics.RelTypes.RELATE_TO, GstAnalytics.ClsMtd
+            ),
+            None,
+        )
+
+        if label_quark and cls_descriptor_mtd is not None:
+            label_id = cls_descriptor_mtd.get_index_by_quark(label_quark)
+            if label_id < 0:
+                raise RuntimeError(
+                    "RegionOfInterest:label_id: Failed to get label id from analytics metadata"
+                )
+            return label_id
+
+        return 0
 
     ## @brief Add new Tensor (inference result) to the RegionOfInterest.
     # @param name Name for the tensor.
     # @param tensor (optional) Tensor object.
     # This function does not take ownership of tensor passed, but only copies its contents
     # @return just created Tensor object, which can be filled with tensor information further
-    def add_tensor(self, name: str = "", tensor: Tensor = None) -> Tensor:
-        tensor_structure = None
-        if tensor:
-            tensor_structure = libgst.gst_structure_copy(tensor._Tensor__structure)
-            if name != "":
-                libgst.gst_structure_set_name(tensor_structure, name.encode('utf-8'))
-        else:
-            tensor_structure = libgst.gst_structure_new_empty(name.encode('utf-8'))
-        libgstvideo.gst_video_region_of_interest_meta_add_param(self.meta(), tensor_structure)
-        return Tensor(tensor_structure)
+    def add_tensor(self, tensor: Tensor):
+        s = tensor.get_structure()
+
+        if not s:
+            raise ValueError("RegionOfInterest:add_tensor: Invalid tensor structure")
+        libgstvideo.gst_video_region_of_interest_meta_add_param(self.meta(), s)
+
+        tensor_mtd = tensor.convert_to_meta(self.__od_meta.meta)
+        if tensor_mtd is not None:
+            if not self.__od_meta.meta.set_relation(
+                GstAnalytics.RelTypes.CONTAIN, self.__od_meta.id, tensor_mtd.id
+            ):
+                raise RuntimeError(
+                    "RegionOfInterest:add_tensor: Failed to set relation to tensor metadata"
+                )
+
+            if not self.__od_meta.meta.set_relation(
+                GstAnalytics.RelTypes.IS_PART_OF, tensor_mtd.id, self.__od_meta.id
+            ):
+                raise RuntimeError(
+                    "RegionOfInterest:add_tensor: Failed to set reverse relation to tensor metadata"
+                )
+
+        self._tensors.append(tensor)
+        if tensor.is_detection():
+            self._detection = tensor
 
     ## @brief Get VideoRegionOfInterestMeta containing bounding box information and tensors (inference results).
     # Tensors are represented as GstStructures added to GstVideoRegionOfInterestMeta.params
@@ -144,35 +269,43 @@ class RegionOfInterest(object):
     def meta(self) -> VideoRegionOfInterestMeta:
         return self.__roi_meta
 
+    ## @brief Get region ID from analytics metadata
+    # @return Region id as an int. Can be a positive or negative integer, but never zero.
+    def region_id(self):
+        return self.__od_meta.id
+
     ## @brief Iterate by VideoRegionOfInterestMeta instances attached to buffer
     # @param buffer buffer with GstVideoRegionOfInterestMeta instances attached
     # @return generator for VideoRegionOfInterestMeta instances attached to buffer
     @classmethod
-    def _iterate(self, buffer: Gst.Buffer):
-        try:
-            meta_api = hash(GObject.GType.from_name("GstVideoRegionOfInterestMetaAPI"))
-        except:
+    def _iterate(cls, buffer: Gst.Buffer):
+        relation_meta = GstAnalytics.buffer_get_analytics_relation_meta(buffer)
+
+        if relation_meta is None:
             return
-        gpointer = ctypes.c_void_p()
-        while True:
-            try:
-                value = libgst.gst_buffer_iterate_meta_filtered(hash(buffer), ctypes.byref(gpointer), meta_api)
-            except:
-                value = None
+
+        for od_mtd in relation_meta.iter_on_type(GstAnalytics.ODMtd):
+            value = None
+            value = libgstvideo.gst_buffer_get_video_region_of_interest_meta_id(
+                hash(buffer), od_mtd.id
+            )
 
             if not value:
-                return
+                raise RuntimeError(
+                    "RegionOfInterest:_iterate: Failed to get VideoRegionOfInterestMeta by id from buffer"
+                )
 
-            roi_meta = ctypes.cast(value, ctypes.POINTER(VideoRegionOfInterestMeta)).contents
-            yield RegionOfInterest(roi_meta)
+            roi_meta = ctypes.cast(
+                value, ctypes.POINTER(VideoRegionOfInterestMeta)
+            ).contents
 
-    ## @brief Construct RegionOfInterest instance from VideoRegionOfInterestMeta. After this, RegionOfInterest will
-    # obtain all tensors (detection & inference results) from VideoRegionOfInterestMeta
-    # @param roi_meta VideoRegionOfInterestMeta containing bounding box information and tensors
-    def __init__(self, roi_meta: VideoRegionOfInterestMeta):
-        self.__roi_meta = roi_meta
+            # roi_meta = GstVideo.buffer_get_video_region_of_interest_meta_id(
+            #     buffer, od_mtd.id
+            # )
 
-    ## @brief Get region ID
-    # @return Region id as an int. Can be a positive or negative integer, but never zero.
-    def region_id(self):
-        return self.__roi_meta.id
+            # if roi_meta is None:
+            #     raise RuntimeError(
+            #         "RegionOfInterest:_iterate: Failed to get VideoRegionOfInterestMeta by id from buffer"
+            #     )
+
+            yield RegionOfInterest(od_mtd, roi_meta)
